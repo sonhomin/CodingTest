9ff0348de5562c261eacd156ffd7f9241e896f6f
손호민 <62591077+sonhomin@users.noreply.github.com>
2021-06-19 23:38:16 +0900

    Update num9.java

--- 코딩테스트3_for문/src/num9.java
+++ 코딩테스트3_for문/src/num9.java
@@ -1,25 +1,19 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class num9 {
-    public static void main(String args[]) throws IOException {
-
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
-        int A;
-        int B;
-
-        StringTokenizer st;
-        for (int i = 1; i <= T; i++) {
-            st = new StringTokenizer(br.readLine()," ");
-            A = Integer.parseInt(st.nextToken());
-            B = Integer.parseInt(st.nextToken());
-            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + (A+B));
-        }
-        br.close();
-    }
-
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+ 
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
 }
